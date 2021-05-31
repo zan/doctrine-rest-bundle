1c5a925ed92e221516a7d03264994fef0fd78fe4
Alex Dean <alex@zanconsulting.com>
2021-05-31 10:02:30 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Zan Doctrine Bundle
+
+Utilities for working with large data sets.
+
+WIP - not for production use
---
+++ src/ZanDoctrineBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zan\DoctrineRestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ZanDoctrineBundle extends Bundle
+{
+
+}
