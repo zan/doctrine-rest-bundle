ee270bebaf2af976770a872526ad76080c628ca2
Alex Dean <alex@zanconsulting.com>
2022-01-05 13:27:30 -0700

    testapp - add doctrine fixtures bundle

--- testapps/sf5.4/config/bundles.php
+++ testapps/sf5.4/config/bundles.php
@@ -4,5 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Zan\DoctrineRestBundle\ZanDoctrineRestBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
