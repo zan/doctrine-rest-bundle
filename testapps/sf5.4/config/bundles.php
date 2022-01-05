72431d775e05d036f85f4bc014d8900a2ae30052
Alex Dean <alex@zanconsulting.com>
2022-01-05 13:39:35 -0700

    testapp - add maker-bundle

--- testapps/sf5.4/config/bundles.php
+++ testapps/sf5.4/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zan\CommonBundle\ZanCommonBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
