47e43b731ebf23fc4eed89c956304993f2286217
Alex Dean <alex@zanconsulting.com>
2022-01-15 11:13:41 -0700

    Add ZanDoctrineCollectionUtils for easily working with collections of entities

---
+++ src/Util/ZanDoctrineCollectionUtils.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Zan\DoctrineRestBundle\Util;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Zan\CommonBundle\Util\ZanEntity;
+
+/**
+ * Utility methods for working with collections of entities
+ */
+class ZanDoctrineCollectionUtils
+{
+    /**
+     * Updates the collection so that it only contains $incomingItems
+     */
+    public static function setItems(Collection $arrayCollection, $setItems)
+    {
+        $toAdd = [];
+        $toRemove = [];
+
+        // Compare incoming items to current items to figure out what should be added
+        foreach ($setItems as $incomingEntity) {
+            $found = false;
+
+            foreach ($arrayCollection as $currEntity) {
+                if (ZanEntity::isSame($incomingEntity, $currEntity)) {
+                    $found = true;
+                    break;
+                }
+            }
+
+            // Not found in existing collection, it needs to be added
+            if (!$found) $toAdd[] = $incomingEntity;
+        }
+
+        // Compare existing items to incoming items to figure out what should be removed
+        foreach ($arrayCollection as $currEntity) {
+            $found = false;
+
+            foreach ($setItems as $incomingEntity) {
+                if (ZanEntity::isSame($currEntity, $incomingEntity)) $found = true;
+            }
+
+            // Current entity wasn't found in the incoming items, it's being removed
+            if (!$found) $toRemove[] = $currEntity;
+        }
+
+        foreach ($toRemove as $entity) {
+            $arrayCollection->removeElement($entity);
+        }
+        foreach ($toAdd as $entity) {
+            $arrayCollection->add($entity);
+        }
+    }
+}
