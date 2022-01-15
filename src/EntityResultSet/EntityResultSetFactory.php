1a52233c49faf14be8646153dfc6835e6e0516d3
Alex Dean <alex@zanconsulting.com>
2022-01-15 11:12:09 -0700

    Add EntityResultSetFactory for easy access to result sets and single entities read via result set

---
+++ src/EntityResultSet/EntityResultSetFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Zan\DoctrineRestBundle\EntityResultSet;
+
+use Doctrine\Common\Annotations\Reader;
+use Doctrine\ORM\EntityManagerInterface;
+use Zan\DoctrineRestBundle\Permissions\PermissionsCalculatorFactory;
+
+/**
+ * Contains helper methods for getting result sets as well as single entities
+ */
+class EntityResultSetFactory
+{
+    private EntityManagerInterface $em;
+
+    private PermissionsCalculatorFactory $permissionsCalculatorFactory;
+
+    private Reader $annotationReader;
+
+    public function __construct(
+        EntityManagerInterface $em,
+        PermissionsCalculatorFactory $permissionsCalculatorFactory,
+        Reader $annotationReader
+    ) {
+        $this->em = $em;
+        $this->permissionsCalculatorFactory = $permissionsCalculatorFactory;
+        $this->annotationReader = $annotationReader;
+    }
+
+    public function mustGetOneReadable(string $entityClassName, $identifier)
+    {
+        $resultSet = new GenericEntityResultSet($entityClassName, $this->em, $this->annotationReader);
+
+        $resultSet->addIdentifierFilter($identifier);
+
+        return $resultSet->mustGetSingleResult();
+    }
+}
