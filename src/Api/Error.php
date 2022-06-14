d35f1c6352b943832ca001b39b98f21e21464302
Alex Dean <alex@zanconsulting.com>
2022-06-14 10:59:48 -0600

    Error.php - add code for entity missing a setter

--- src/Api/Error.php
+++ src/Api/Error.php
@@ -6,8 +6,9 @@ class Error
 {
     public const GENERIC_ERROR              = 'Zan.Drest.ApiException';
 
-    // No permissions are defined on the entity
+    // Entity-specific errors
     public const NO_ENTITY_PERMISSIONS       = 'Zan.Drest.NoPermissionsOnEntity';
+    public const NO_ENTITY_PROPERTY_SETTER   = 'Zan.Drest.NoEntityPropertySetter';
 
     // The entity version in the database is newer than the entity version sent by the client
     public const CONFLICTING_EDITS          = 'Zan.Drest.ConflictingEdits';
