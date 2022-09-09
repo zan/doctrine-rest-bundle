6018168d9f12b8fe2eb43b48d09b6223e4d79cc5
Alex Dean <alex@zanconsulting.com>
2022-09-09 11:15:44 -0600

    Add EntityPermissionsController

---
+++ src/Controller/EntityPermissionsController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Zan\DoctrineRestBundle\Controller;
+
+use Doctrine\Common\Annotations\Reader;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Zan\CommonBundle\Util\RequestUtils;
+use Zan\DoctrineRestBundle\Api\Error;
+use Zan\DoctrineRestBundle\EntityResultSet\GenericEntityResultSet;
+use Zan\DoctrineRestBundle\Permissions\PermissionsCalculatorFactory;
+use Zan\DoctrineRestBundle\Response\ApiErrorResponse;
+use Zan\DoctrineRestBundle\Response\ApiResponse;
+use Zan\DoctrineRestBundle\Util\DoctrineRestUtils;
+
+/**
+ * Provides permissions information about an entity class or a specific entity
+ * 
+ * @Route("/entity-permissions")
+ */
+class EntityPermissionsController extends AbstractController
+{
+    /**
+     * Checking an entity class name without an identifier - check create permissions
+     *
+     * @Route("/{entityId}", methods={"GET"})
+     */
+    public function getGeneralEntityPermissions(
+        string $entityId,
+        Request $request,
+        PermissionsCalculatorFactory $permissionsCalculatorFactory,
+    ): JsonResponse
+    {
+        $entityClassName = DoctrineRestUtils::unescapeEntityId($entityId);
+        $params = RequestUtils::getParameters($request);
+
+        $permissionsCalculator = $permissionsCalculatorFactory->getPermissionsCalculator($entityClassName);
+        if (!$permissionsCalculator) return new ApiErrorResponse(Error::NO_ENTITY_PERMISSIONS, 'No permissions calculator is defined for ' . $entityId);
+
+        $permissions = [
+            'create' => $permissionsCalculator->canCreateEntity($entityClassName, $this->getUser(), $params),
+        ];
+
+        return new ApiResponse($permissions);
+    }
+
+    /**
+     * Retrieve permissions information about a specific entity
+     *
+     * @Route("/{entityId}/{identifier}", methods={"GET"})
+     */
+    public function getSpecificEntityPermissions(
+        string $entityId,
+        string $identifier,
+        Request $request,
+        EntityManagerInterface $em,
+        Reader $annotationReader,
+        PermissionsCalculatorFactory $permissionsCalculatorFactory,
+    ): JsonResponse
+    {
+        $entityClassName = DoctrineRestUtils::unescapeEntityId($entityId);
+        $params = RequestUtils::getParameters($request);
+
+        $permissionsCalculator = $permissionsCalculatorFactory->getPermissionsCalculator($entityClassName);
+        if (!$permissionsCalculator) return new ApiErrorResponse(Error::NO_ENTITY_PERMISSIONS, 'No permissions calculator is defined for ' . $entityId);
+
+        // Retrieve the entity from the database
+        $resultSet = new GenericEntityResultSet($entityClassName, $em, $annotationReader);
+        $resultSet->addIdentifierFilter($identifier);
+
+        $entity = $resultSet->getSingleResult();
+        if (!$entity) {
+            return new ApiErrorResponse(
+                Error::NO_ENTITY,
+                sprintf(
+                    'No entity found with class "%s" and identifier "%s"',
+                    $entityClassName,
+                    $identifier,
+                ),
+                404,
+            );
+        }
+
+        $permissions = [
+            'edit' => $permissionsCalculator->canEditEntity($entity, $this->getUser()),
+        ];
+
+        return new ApiResponse($permissions);
+    }
+}
