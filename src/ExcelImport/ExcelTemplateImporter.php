1490bfd241e6fcc8f7cb383c639f5354b49c5aa9
Alex Dean <alex@zanconsulting.com>
2024-07-10 11:28:44 -0600

    ExcelTemplateImporter - attempt to detect empty rows at the end of the file and do not try to import them

--- src/ExcelImport/ExcelTemplateImporter.php
+++ src/ExcelImport/ExcelTemplateImporter.php
@@ -16,8 +16,37 @@ class ExcelTemplateImporter
             return;
         }
 
-        // Iterate 1-based rows starting at row 2 (skip header row)
+        // Pass 1 - figure out where the real end row is
+        // This allows having extra columns in the Excel file that are effectively ignored during import even if
+        // They have extra data in them
+        $lastRowIndex = 0;
         foreach ($worksheet->getRowIterator(2) as $row) {
+            $cellIterator = $row->getCellIterator();
+            // Iterate all cells regardless of whether they have values so we stay in sync with columns
+            $cellIterator->setIterateOnlyExistingCells(false);
+
+            $colIdx = 0; // 0-based for working with the php columns array
+            foreach ($cellIterator as $cell) {
+                // Ignore extra columns
+                if ($colIdx > count($columnKeys) - 1) continue;
+                $colIdx++;
+
+                // Consider it a valid row if there's anything "non-empty" in the cell
+                if (
+                    $cell->getValue() !== null
+                    && $cell->getValue() !== ''
+                ) {
+                    $lastRowIndex = $row->getRowIndex();
+                    break;
+                }
+            }
+        }
+
+        // Pass 2 - Read data by iterating 1-based rows starting at row 2 (skip header row)
+        foreach ($worksheet->getRowIterator(2) as $row) {
+            // Stop iterating if we've hit the end of valid data (see pass 1 above)
+            if ($row->getRowIndex() > $lastRowIndex) break;
+
             $cellIterator = $row->getCellIterator();
             // Iterate all cells regardless of whether they have values so we stay in sync with columns
             $cellIterator->setIterateOnlyExistingCells(false);
